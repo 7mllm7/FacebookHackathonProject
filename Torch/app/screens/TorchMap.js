1b35a3982d1300cd00420a1ecdfc928633f74df4
ML <moti.l@mediapplications.com>
2016-06-23 13:12:20 +0300

    Map with lines

--- Torch/app/screens/TorchMap.js
+++ Torch/app/screens/TorchMap.js
@@ -1,11 +1,58 @@
 import React                      from 'react';
 import { View, Text } from "react-native";
+import MainMap from '../components/MainMap.js'
 
 class TorchMap extends React.Component {
 
+  
   render() {
     return (
-      <View><Text>Map</Text></View>
+      <MainMap
+          runs={[
+            {
+              name: "My First Run",
+              origin: {
+                  longitude: 29.8,
+                  latitude: 30.0
+              },
+              destination: {
+                longitude: 30.1,
+                latitude: 30.1
+              }
+            },
+            {
+              name: "My Second Run",
+                origin: {
+                  longitude: 30.1,
+                  latitude: 30.1
+                },
+                destination: {
+                  longitude: 29.9,
+                  latitude: 29.8
+                }
+            },
+            {
+              name: "My Third Run",
+              origin: {
+                  longitude: 29.9,
+                  latitude: 29.8
+              },
+              destination: {
+                longitude: 30.7,
+                latitude: 30.4
+              }
+            }
+          ]}
+          torch={
+            {
+              name: "My Torch!",
+              location: {
+                  longitude: 30.7,
+                  latitude: 30.4
+              }
+            }
+          }
+      />
     );
   }
 }
