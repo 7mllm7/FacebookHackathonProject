12b1b32fa815cccae1797e4a3a5a56139a0fb408
ML <moti.l@mediapplications.com>
2016-06-23 16:21:07 +0300

    torch real backend

--- Torch/app/services/torch.js
+++ Torch/app/services/torch.js
@@ -2,6 +2,8 @@
  * Created by MLmediapps on 23/06/2016.
  */
 
+import * as Backend from 'backend'
+
 function delay(n) {
     return new Promise((resolve, reject) => {
         setTimeout(() => resolve(), n);
@@ -9,48 +11,52 @@ function delay(n) {
 }
 
 export async function fetchAll() {
-    await delay(2000);
-    return [
-        {
-            id: "verylovelytorchid123",
-            name: "My Torch!",
-            runner: null,
-            currentLocation: {
-                longitude: 30.7,
-                latitude: 30.4
-            },
-            currentRun : null
-        }
-    ];
+    return await Backend.fetchGET('torches');
+    // await delay(2000);
+    // return [
+    //     {
+    //         id: "verylovelytorchid123",
+    //         name: "My Torch!",
+    //         runner: null,
+    //         currentLocation: {
+    //             longitude: 30.7,
+    //             latitude: 30.4
+    //         },
+    //         currentRun : null
+    //     }
+    // ];
 }
 
 export async function pickup(torchId, userId, location) {
-    await delay(2000);
-    return [
-        {
-            id: "verylovelytorchid123",
-            name: "My Torch!",
-            currentLocation: location,
-            currentRun: {
-                name: "My Current Run",
-                runner: userId,
-                origin: {
-                    longitude: 30.7,
-                    latitude: 30.4
-                }
-            }
-        }
-    ];
+    return await Backend.fetchPOST(`torch/${torchId}/pickup`, {location, userId});
+
+    // await delay(2000);
+    // return [
+    //     {
+    //         id: "verylovelytorchid123",
+    //         name: "My Torch!",
+    //         currentLocation: location,
+    //         currentRun: {
+    //             name: "My Current Run",
+    //             runner: userId,
+    //             origin: {
+    //                 longitude: 30.7,
+    //                 latitude: 30.4
+    //             }
+    //         }
+    //     }
+    // ];
 }
 
 export async function drop(torchId, userId, location) {
-    await delay(2000);
-    return [
-        {
-            id: "verylovelytorchid123",
-            name: "My Torch!",
-            currentLocation: location,
-            currentRun: null
-        }
-    ];
+    return await Backend.fetchPOST(`torch/${torchId}/drop`, {location, userId});
+    // await delay(2000);
+    // return [
+    //     {
+    //         id: "verylovelytorchid123",
+    //         name: "My Torch!",
+    //         currentLocation: location,
+    //         currentRun: null
+    //     }
+    // ];
 }
